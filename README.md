b2f455e7229ca2dc7ff64a7fcc56c362c63c5133
Cong Huynh <hnc203204@gmail.com>
2024-03-28 14:29:34 +0700

    first update

---
+++ README.md
@@ -0,0 +1,24 @@
+# Technology Blog Website.
+
+## Thành viên
+
+| MÃ SINH VIÊN | HỌ TÊN |
+| ------------ | ------ |
+| 22022565 | Nguyễn Công Huynh |
+| 22022570 | Lèng Hữu Phúc |
+| 22022571 | Nguyễn Văn Trường |
+|22022652 | Ngô Đức Hùng |
+
+
+## Running this project
+
+```
+pip install -r requirements.txt 
+```
+
+```
+python main.py
+```
+
+
+
--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_mamage = LoginManager()
