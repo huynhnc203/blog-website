00815961f3400b6ce379419aa1aaabba698a220b
HwuuPhuc0904 <lhuuphuc2004@gmail.com>
2024-04-22 21:37:46 +0700

    add components and update front-end

---
+++ front-end/src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Homepage.css'
+
+const Homepage = () => {
+    return(
+        <div>
+            <div className="banner">
+                <div style={{paddingLeft: '120px'}}>
+                    <h1>World of Technology</h1>
+                    <p>Discover stories, thinking, and expertise from writers on any topic.</p>
+                </div>
+                <div className="banner-img-box">
+                    <img src = ""  id = "bannerImg"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
