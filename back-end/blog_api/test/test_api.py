418486f624e9ce1d5c1f2f636b1037322845368b
Cong Huynh <hnc203204@gmail.com>
2024-04-29 21:51:26 +0700

    update user_management, post_management, infrastructure, response

---
+++ back-end/blog_api/test/test_api.py
@@ -0,0 +1,23 @@
+from blog_api import db
+from blog_api.test.basic_test import BasicTestCase
+from flask_restful import Api
+import os
+from abc import ABC
+
+class TestAPI(BasicTestCase, ABC):
+    def setUp(self) -> None:
+        super().setUp()
+        api = Api(self.app)
+        db.init_app(self.app)
+        with self.app.app_context():
+            db.create_all()
+        from blog_api.users.management.user_management import UserManagement
+        from blog_api.posts.posts_management.posts_mamagement import PostManagement
+        api.add_resource(UserManagement, "/api/users", "/api/user/<int:id>")
+        api.add_resource(PostManagement, "/api/posts", "/api/posts/<int:id>")
+        
+
+        self.client = self.app.test_client()
+
+    def tearDown(self) -> None:
+        os.remove(self.test_db_file)
