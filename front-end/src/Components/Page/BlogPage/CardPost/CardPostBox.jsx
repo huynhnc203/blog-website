fec7a6a362ca1fb610201728ff972a9a079428aa
HwuuPhuc0904 <lhuuphuc2004@gmail.com>
2024-04-29 16:39:39 +0700

    update cardpostblog for blogpage

---
+++ front-end/src/Components/Page/BlogPage/CardPost/CardPostBox.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  Img,
+  Flex,
+  Center,
+  useColorModeValue,
+  HStack,
+} from '@chakra-ui/react';
+import { BsArrowUpRight, BsHeartFill, BsHeart } from 'react-icons/bs';
+
+const  CardPostBox =() => {
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <Center py={6}>
+      <Box
+        w="300px"
+        rounded={'lg'}
+        my={3}
+        overflow={'hidden'}
+        bg="white"
+        border={'1px'}
+        borderColor="black"
+        boxShadow={useColorModeValue('6px 6px 0 black', '6px 6px 0 cyan')}>
+        <Box h={'200px'} borderBottom={'1px'} borderColor="black">
+          <Img
+            src={
+              'https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+            }
+            roundedTop={'sm'}
+            objectFit="cover"
+            h="full"
+            w="full"
+            alt={'Blog Image'}
+          />
+        </Box>
+        <Box p={4}>
+          <Box
+            display={'inline-block'}
+            px={2}
+            py={1}
+            color="whrite"
+            mb={2}>
+            <Text fontSize={'xs'} fontWeight="medium" color={'green'}>
+              Front-end
+            </Text>
+          </Box>
+          <Heading color={'black'} fontSize={'2xl'} /*gioi han so dong hien thi*/ noOfLines={1}>
+            React v18.0
+          </Heading>
+          <Text color={'gray.500'} noOfLines={2}>
+            {/* chỗ để title */}
+            Bài viết hay về reac , thú vị và nhiều thứ khàm phá quá tuyệt vời
+          </Text>
+        </Box>
+        <HStack borderTop={'1px'} color="black">
+          <Flex
+            p={4}
+            alignItems="center"
+            justifyContent={'space-between'}
+            roundedBottom={'lg'}
+            cursor={'pointer'}
+            w="full">
+            <Text fontSize={'md'} fontWeight={'semibold'}>
+              View more
+            </Text>
+            <BsArrowUpRight />
+          </Flex>
+          <Flex
+            p={4}
+            alignItems="center"
+            justifyContent={'space-between'}
+            roundedBottom={'sm'}
+            borderLeft={'1px'}
+            cursor="pointer"
+            onClick={() => setLiked(!liked)}>
+            {liked ? (
+              <BsHeartFill fill="red" fontSize={'24px'} />
+            ) : (
+              <BsHeart fontSize={'24px'} />
+            )}
+          </Flex>
+        </HStack>
+      </Box>
+    </Center>
+  );
+}
+export default CardPostBox
