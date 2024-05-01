9ae36592f5f400c115f3df3156041a2a2a129453
ngoduchung04 <nevsmi04@gmail.com>
2024-05-01 22:21:14 +0700

    update ui

--- front-end/src/Components/Homepage/Homepage.jsx
+++ front-end/src/Components/Homepage/Homepage.jsx
@@ -2,38 +2,38 @@ import React from "react";
 import './Homepage.css'
 import { Button } from "react-bootstrap";
 import FeaturePost from "./HomepageTwo/FeaturePost";
-import Learmore from "./HomepageThree/Learnmore" ;
+import Learmore from "./HomepageThree/Learnmore";
 import DevelopmentTeam from "./HomepageFour/DevelopmentTeam";
 import GridEvaluatePage from "./EvaluatePage/EvaluatePage";
 import Footer from "./Footer/Footer";
 
 const Homepage = () => {
-    return(
+    return (
         <>
-        <div>
-        <div className="banner" style={{ backgroundImage: `url("baner2.png")` }}>   
-                <div style={{paddingLeft: '120px'}}>
-                    <h1>World of Technology</h1>
-                    <p>Discover stories, thinking, and expertise from writers on any topic.</p>
-                    <button type="button" class="btn btn-lg rounded-5 custom-button">Get started</button>
+            <div>
+                <div className="banner" style={{ backgroundImage: `url("baner2.png")` }}>
+                    <div style={{ paddingLeft: '120px' }}>
+                        <h1>World of Technology</h1>
+                        <p style={{ lineHeight: '1em' }}>Discover stories, thinking, and expertise from writers on any topic.</p>
+                        <button type="button" class="btn btn-lg rounded-5 custom-button">Get started</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="feature-post-container">
-         <FeaturePost/> 
-        </div>
-        <div className = "Learn">
-            <Learmore/>
-        </div>
-        <div>
-            <DevelopmentTeam/>
-        </div>
-        <div>
-            <GridEvaluatePage/>
-        </div>
-        <div>
-            <Footer/>
-        </div>
+            <div className="feature-post-container">
+                <FeaturePost />
+            </div>
+            <div className="Learn">
+                <Learmore />
+            </div>
+            <div>
+                <DevelopmentTeam />
+            </div>
+            <div>
+                <GridEvaluatePage />
+            </div>
+            <div>
+                <Footer />
+            </div>
 
         </>
     )
--- front-end/src/Components/Homepage/HomepageTwo/CardBlog.jsx
+++ front-end/src/Components/Homepage/HomepageTwo/CardBlog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 import { Flex, Box, Heading, Text, Avatar, IconButton, Button } from "@chakra-ui/react";
 import { BsThreeDotsVertical } from "react-icons/bs";
@@ -6,16 +6,44 @@ import { BiLike, BiChat, BiShare } from "react-icons/bi";
 import { Image } from "@chakra-ui/react";
 
 const CardBlog = () => {
+    const [isTitleHover, setIsTitleHover] = useState(false);
+    const [isUserHover, setIsUserHover] = useState(false);
+    const [likeCount, setLikeCount] = useState(0);
+    const [liked, setLiked] = useState(false);
+
+    const handleTitleHover = () => {
+        setIsTitleHover(!isTitleHover);
+    };
+
+    const handleUserHover = () => {
+        setIsUserHover(!isUserHover);
+    };
+
+    const handleLikeClick = () => {
+        if (!liked) {
+            setLikeCount(likeCount + 1);
+            setLiked(true);
+        } else {
+            setLikeCount(likeCount - 1);
+            setLiked(false);
+        }
+    };
     return (
         <Card maxW='md'>
             <CardHeader>
                 <Flex spacing='4'>
                     <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                        <Avatar name='Hung' src='hunggay.jpg' />
+                        <Avatar name='Hung' src='hunggay.jpg' style={{ marginTop: '-20px' }} />
 
                         <Box>
-                            <Heading size='sm'>Hung Depzai</Heading>
-                            <Text>hi ae</Text>
+                            <Heading
+                                size='sm'
+                                style={{ marginTop: '0px', marginBottom: '-6px' }}
+                                onMouseEnter={handleUserHover}
+                                onMouseLeave={handleUserHover}
+                                cursor={"pointer"}
+                                color={isUserHover ? "blue.500" : "black"}>Hung Depzai</Heading>
+                            <Text style={{ marginBottom: '20px' }}>Viết báo dạo</Text>
                         </Box>
                     </Flex>
                     <IconButton
@@ -27,9 +55,15 @@ const CardBlog = () => {
                 </Flex>
             </CardHeader>
             <CardBody>
-                <Text>
-                    Tôi là một deverlopber chuyên nghiệp, tiếng anh thành thạo hi, hello, sở thích nghe nhạc dzz và ngồi hát.
-                    Trang web đỉnh của chóp do Phúc đẹp trai vĩ đại xây dựng này thực sự ấn tượng đối với tôi.
+                <Text
+                    style={{ marginTop: '-60px', marginBottom: '40px' }}
+                    className="bold-text"
+                    color='Black'>
+                    Lời giới thiệu
+                </Text>
+                <Text style={{ marginTop: '-40px', marginBottom: '0px' }}>
+                    Tôi là một developer chuyên nghiệp, tiếng anh thành thạo hi, hello, sở thích nghe nhạc vàng và ngồi hát.
+                    Trang web đỉnh của chóp do Phúc và tôi - đẹp trai vĩ đại xây dựng này thực sự rất tốt rất tốt.
                     Một trang web mang đầy đủ tinh hoa của một svip.
                 </Text>
             </CardBody>
@@ -48,8 +82,15 @@ const CardBlog = () => {
                     },
                 }}
             >
-                <Button flex='1' variant='ghost' leftIcon={<BiLike />}>
-                    Like
+                <Button
+                    className={`like-button ${liked ? "liked" : ""}`}
+                    style={{ color: liked ? 'blue' : 'black' }}
+                    variant='ghost'
+                    leftIcon={<BiLike />}
+                    onClick={handleLikeClick}
+                    size="md"
+                >
+                    {liked ? "Unlike" : "Like"} ({likeCount})
                 </Button>
                 <Button flex='1' variant='ghost' leftIcon={<BiChat />}>
                     Comment
