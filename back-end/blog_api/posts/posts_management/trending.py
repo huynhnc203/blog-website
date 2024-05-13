f00839940d14bb25a87ff2e6305c3fcf9a1edb0c
Cong Huynh <hnc203204@gmail.com>
2024-05-14 00:51:59 +0700

    add trending tag

--- back-end/blog_api/posts/posts_management/trending.py
+++ back-end/blog_api/posts/posts_management/trending.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, request
+from flask import Blueprint
 from blog_api.utils.responses import response_with
 import blog_api.utils.responses as resp
 from blog_api.utils.utils import handle_exceptions
 from blog_api.models.posts_model import BlogPost
 
-trending_bp = Blueprint("trending", __name__)
+post_trending_bp = Blueprint("trending", __name__)
 
-@trending_bp.route("/trending", methods=["GET"])
-@trending_bp.route("/trending/<int:page>", methods=["GET"])
+@post_trending_bp.route("/trending", methods=["GET"])
+@post_trending_bp.route("/trending/<int:page>", methods=["GET"])
 @handle_exceptions
 def get_trending_posts(page=1):
     posts = BlogPost.query.order_by(BlogPost.likes.desc()).paginate(page, 5)
