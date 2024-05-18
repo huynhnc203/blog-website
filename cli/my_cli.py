9c2b595d44764869f184ce601a2c1e6e66f98e03
Cong Huynh <hnc203204@gmail.com>
2024-05-19 02:09:18 +0700

    update cli

--- cli/my_cli.py
+++ cli/my_cli.py
@@ -7,17 +7,14 @@ def cli():
 @cli.command()
 def runserver():
     """Run the Flask server."""
-    from subprocess import run
-    run(['python3 server.py'], cwd='../back-end', shell=True)
+    from subprocess import Popen
+    Popen(['flask', 'run'], cwd='../back-end', shell=True)
 
 @cli.command()
 def build():
     """Build the React frontend."""
     from subprocess import run
-    run(['npm start'], cwd='../front-end', shell=True)
+    run(['npm', 'run', 'build'], cwd='../front-end', shell=True)
 
 if __name__ == '__main__':
     cli()
-
-
-    
