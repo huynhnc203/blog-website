92f73cbf0481a3fde41e72eb2e008d803556d39c
Cong Huynh <hnc203204@gmail.com>
2024-05-19 02:07:37 +0700

    update cli

--- cli/my_cli.py
+++ cli/my_cli.py
@@ -8,14 +8,12 @@ def cli():
 def runserver():
     """Run the Flask server."""
     from subprocess import run
-    run(['pip install -r requirements.txt'], cwd='../back-end', shell=True)
     run(['python3 server.py'], cwd='../back-end', shell=True)
 
 @cli.command()
 def build():
     """Build the React frontend."""
     from subprocess import run
-    run(['npm install -force'], cwd='../front-end', shell=True)
     run(['npm start'], cwd='../front-end', shell=True)
 
 if __name__ == '__main__':
